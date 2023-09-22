31c7112ba2f661c6dd93a48ed64144ad1174cfd3
Eugen <119263405+enbt0909@users.noreply.github.com>
2023-09-22 19:30:38 +0200

    first

---
+++ jsSterne/README.md
@@ -0,0 +1,3 @@
+# jsSterne
+
+enter
---
+++ jsSterne/script.js
@@ -0,0 +1,10 @@
+let star1 = "  *  ";
+let star2 = " *** ";
+let star3 = "*****";
+
+document.write(star1);
+document.write(star2);
+document.write(star3);
+document.write(star2);
+document.write(star1);
+
